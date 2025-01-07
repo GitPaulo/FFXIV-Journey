8bcb6573965cc2781316cf6c4717f95721d2bb8d
GitPaulo <paulo.santos98@gmail.com>
2025-01-07 20:46:02 +0000

    use $base for path

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,9 +2,10 @@
 export const ssr = false;
 export const prerender = true;
 
+import { base } from "$app/paths";
 import type { ExpansionsQuests } from "$lib/model.js";
 export async function load({ fetch }): Promise<{ quests: ExpansionsQuests }> {
-  const response = await fetch("/Quests.json");
+  const response = await fetch(`${base}/Quests.json`);
   if (!response.ok) {
     throw new Error("Failed to fetch quests");
   }
